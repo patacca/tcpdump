df55edaf5cc7d40c1dc816c42e4637bda697d4ce
patacca <patacca@autistici.org>
2020-12-13 18:15:39 +0100

    Adding code to be able to compile even not with afl-clang

--- tcpdump.c
+++ tcpdump.c
@@ -1481,7 +1481,20 @@ open_interface(const char *device, netdissect_options *ndo, char *ebuf)
 
 
 
+/* this lets the source compile without afl-clang-fast/lto */
+#ifndef __AFL_FUZZ_TESTCASE_LEN
 
+int           fuzz_len;
+unsigned char fuzz_buf[1024000];
+
+  #define __AFL_FUZZ_TESTCASE_LEN fuzz_len
+  #define __AFL_FUZZ_TESTCASE_BUF fuzz_buf
+  #define __AFL_FUZZ_INIT() void sync(void);
+  #define __AFL_LOOP(x) \
+    ((fuzz_len = read(0, fuzz_buf, sizeof(fuzz_buf))) > 0 ? 1 : 0)
+  #define __AFL_INIT() sync()
+
+#endif
 
 __AFL_FUZZ_INIT();
 
